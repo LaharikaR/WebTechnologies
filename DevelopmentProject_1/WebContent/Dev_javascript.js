3a52fb7f04c3fe765d50e7f2ad981baec134ee59
home <home@lenovo>
2019-03-04 22:00:05 -0500

    Edited the funtion

--- DevelopmentProject_1/WebContent/Dev_javascript.js
+++ DevelopmentProject_1/WebContent/Dev_javascript.js
@@ -4,8 +4,5 @@
 
 function myFunction() { 
 	  document.getElementById("myname").innerHTML = "Laharika Ravula";
-	  document.getElementById("demo").style.fontSize = "25px"; 
-	  document.getElementById("demo").style.color = "red";
-	  document.getElementById("demo").style.backgroundColor = "yellow";
 	}
 
