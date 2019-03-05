832b00afc78e360cc05905037276dd32a5b6c4f1
home <home@lenovo>
2019-03-04 21:55:32 -0500

    Javasript for development project 1

---
+++ DevelopmentProject_1/WebContent/Dev_javascript.js
@@ -0,0 +1,11 @@
+/**
+ * the below funtion is to display my name
+ */
+
+function myFunction() { 
+	  document.getElementById("myname").innerHTML = "Laharika Ravula";
+	  document.getElementById("demo").style.fontSize = "25px"; 
+	  document.getElementById("demo").style.color = "red";
+	  document.getElementById("demo").style.backgroundColor = "yellow";
+	}
+
