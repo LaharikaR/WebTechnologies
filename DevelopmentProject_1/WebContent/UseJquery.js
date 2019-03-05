22de344d50dbf0abf9a9b53b4c1c30c4b3649a7f
home <home@lenovo>
2019-03-05 17:42:06 -0500

    New file for .click function

---
+++ DevelopmentProject_1/WebContent/UseJquery.js
@@ -0,0 +1,26 @@
+/**
+ * use of .click() in jquery
+ */
+
+$(document).ready(function(){
+
+/*Function to open Summary page*/
+$( "#index_summary" ).click(function() {
+document.location.href="summary.html";
+});
+
+/*Function to open Education page*/
+$( "#index_education" ).click(function() {
+document.location.href="Education.html";
+});
+
+/*Function to open Experience page*/
+$( "#index_experience" ).click(function() {
+document.location.href="Experience.html";
+});
+/*Function to open Contact page */
+$( "#index_contact" ).click(function() {
+	document.location.href="Contact.html";
+	});
+
+});
